59cf8bc403a940b0f61951a36d3477708e9b2eed
Nghi Nguyen Van <nghinv@exoplatform.com>
2013-08-23 15:05:11 +0700

    improve script naming in PLF4.1.x

--- scripts/PLF41x/x.sh
+++
@@ -1,2 +0,0 @@
-pushd /home/qahudson/testsuite && bash ./jmeter-utils-testcontroller.sh START "${g_conf_file}_`hostname`"
-popd
--- scripts/PLF41x/y.sh
+++
@@ -1,2 +0,0 @@
-pushd /home/qahudson/testsuite && bash ./jmeter-utils-testcontroller.sh STOP_APPLICATION "${g_conf_file}_`hostname`"
-popd
